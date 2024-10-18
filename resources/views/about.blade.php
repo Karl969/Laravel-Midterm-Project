8be57bb1eb26f9cfd6a33ac0f3a9eba13c57a525
Karl969 <158837807+Karl969@users.noreply.github.com>
2024-10-18 20:49:37 +0800

    Changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,30 +5,6 @@
     <link rel="stylesheet" href="{{ asset('assets/css/Webpage.css') }}">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="{{ route('welcome') }}">Home</a></li>
-        </ul>
-    </nav>
-
-    <h1>About Us</h1>
-
-    <img src="{{ asset('assets/images/1925_FORD.jpg') }}" alt="1925 Ford car">
-
-    <p>
-        "CAR LOSE AUTOMOBILES" is a leading automotive company dedicated to providing the best car information on the internet. We are passionate about cars and strive to offer our visitors valuable insights, reviews, and news.
-    </p>
-
-    <p>
-        Our team of automotive experts has years of experience in the industry and is committed to delivering accurate and up-to-date information. We believe that knowledge is power, and we want to empower our visitors to make informed decisions about their automotive needs.
-    </p>
-
-    <p>
-        Whether you're a car enthusiast, a potential buyer, or simply curious about the world of automobiles, we have something for you. Explore our website to discover a wealth of information on car models, reviews, news, and more.
-    </p>
-
-    <footer>
-        &copy; 2023 CAR LOSE AUTOMOBILES
-    </footer>
+  
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,45 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Homepage</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Lessons</title>
     <link rel="stylesheet" href="{{ asset('assets/css/Webpage.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 
-<nav>
-    <ul>
-        <li><a href="{{ route('About') }}">About Us</a></li>
-        <li><a href="{{ route('Content') }}">Content</a></li>
-    </ul>
-</nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">My Laravel App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/middleware">Middleware</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/routes">Routes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/homepage">Homepage</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/portfolio">Portfolio</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-    <section id="home">
-        <h1>Homepage</h1>
-        <img src="{{ asset('assets/images/1925_FORD.jpg') }}" alt="1925 Ford car">
-        <p>Discover the world of automobiles.</p>
-    </section>
-
-    <section id="about">
-        <h2>About Cars</h2>
-        <p>Cars are vehicles that travel on roads. They come in various types, from small city cars to large SUVs and luxury sedans.</p>
-    </section>
-
-    <section id="types">
-        <h2>Types of Cars</h2>
-        <ul>
-            <li>Sedans</li>
-            <li>SUVs</li>
-            <li>Sports cars</li>
-            <li>Trucks</li>
-            <li>Minivans</li>
-            <li>Hybrids</li>
-            <li>Electric cars</li>
+    <div class="container mt-5">
+        <h1>Lessons Learned About Laravel</h1>
+            </li>
         </ul>
-    </section>
-
-    <footer>
-        &copy; 2024 CAR LOSE AUTOMOBILES
-    </footer>
+    </div>
 </body>
 </html>
